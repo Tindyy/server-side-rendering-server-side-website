e7fcef48b0c1a625565f7b831fb1c138066dc35f
Tindyy <vdgraaf.tessa@gmail.com>
2025-03-10 12:32:49 +0100

    404 #4

--- server.js
+++ server.js
@@ -72,7 +72,11 @@ app.get('/art/:id/', async function (request, response){
 
 
 
+//404 handle
 
+app.use((request, response, next) => {
+  response.status(404).render("404.liquid")
+})
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
