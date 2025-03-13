a12ee3631c014101c72c8a3381122641558029bd
Tindyy <vdgraaf.tessa@gmail.com>
2025-03-13 22:15:15 +0100

    new homepage design #4

--- public/script/qatarmuseum.js
+++ public/script/qatarmuseum.js
@@ -1,5 +1,19 @@
-window.onload = function {
-    $body = $('body')
-    $fullscreenbtn = $('fullscreen')
-    $smallscreenbtn = $('smallscreen')
-};
+const allObjects = document.querySelector('.all-objects');
+const fullscreenBtn = document.querySelector('#fsinactive'); // Fullscreen button
+const smallscreenBtn = document.querySelector('#fsactive'); // Smallscreen button
+const homepage = document.querySelector('.home-page')
+
+fullscreenBtn.addEventListener('onClick', () => {
+    fullscreenBtn.classList.add('active');
+    smallscreenBtn.classList.add('active');
+    allObjects.classList.add('active');
+    homepage.classList.add('active');
+});
+
+smallscreenBtn.addEventListener('click', () => {
+    fullscreenBtn.classList.remove('active');
+    smallscreenBtn.classList.remove('active');
+    allObjects.classList.remove('active');
+    homepage.classList.remove('active');
+});
+
